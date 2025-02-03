841c30a6be5d0c04d76e15b624459b5c6e3cbab2
Fredy <freddyjair753@gmail.com>
2025-02-03 17:19:55 -0600

    Forzar protocolo https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,11 @@ use App\Http\Middleware\Session_Middleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
+
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://gestorinventario-production.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
